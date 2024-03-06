ade48dad1649ec771f18e974e4bd37da6c9bc563
miguel sanglard <miguelsanglard@gmail.com>
2024-03-06 12:11:52 -0300

    loops e iterable

---
+++ loops e iterable/script.js
@@ -0,0 +1,11 @@
+const listas = document.querySelectorAll('li')
+
+for (const lista of listas) {
+  lista.classList.add('classe')
+}
+
+
+
+for (const windowKey in window) {
+  console.log(windowKey)
+}
